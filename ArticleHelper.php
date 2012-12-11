1cbb848d734c4b14bb40da39ce15527f1c97f140
Konrad Abicht <k.abicht@googlemail.com>
2012-12-11 13:26:51 +0100

    Show article iff model was selected and is editable

--- ArticleHelper.php
+++ ArticleHelper.php
@@ -7,27 +7,36 @@ class ArticleHelper extends OntoWiki_Component_Helper
      */
     public function init()
     {
-        //Ontowiki Navigation
-        $navigation = OntoWiki::getInstance()->getNavigation();
-
-        // get current request info
-        $request  = Zend_Controller_Front::getInstance()->getRequest();
-        $controller = $request->getControllerName();
-        $action = $request->getActionName();
+        // Show tab only if model is selected and editable
+        $modelIsSelected = null != OntoWiki::getInstance()->selectedModel;
         
-        // set standard priority
-        $standardPriority = $this->_privateConfig->get('standardPriority');
+        if( true == $modelIsSelected ) {
+            
+            $modelIsEditable = OntoWiki::getInstance()->selectedModel->isEditable();
+            
+            if ( true == $modelIsEditable  ) {
+                //Ontowiki Navigation
+                $navigation = OntoWiki::getInstance()->getNavigation();
 
-        if(($controller == 'article' && $action == 'edit')){
-            $standardPriority = 20;
+                // get current request info
+                $request  = Zend_Controller_Front::getInstance()->getRequest();
+                $controller = $request->getControllerName();
+                $action = $request->getActionName();
+                
+                // set standard priority
+                $standardPriority = $this->_privateConfig->get('standardPriority');
+
+                if(($controller == 'article' && $action == 'edit')){
+                    $standardPriority = 20;
+                }
+                
+                $navigation->register('article', array(
+                    'controller' => 'article',     
+                    'action'     => 'edit',        
+                    'name'       => 'Article',
+                    'priority'   => $standardPriority
+                ));
+            }
         }
-        
-        $navigation->register('article', array(
-            'controller' => 'article',     
-            'action'     => 'edit',        
-            'name'       => 'Article',
-            'priority'   => $standardPriority
-        ));
-
     }
 }
