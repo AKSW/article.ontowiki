0cc5ca60012b0283c293cb7872932f230365cc11
Lars Eidam <larseidam@googlemail.com>
2012-12-11 14:47:22 +0100

    - fix issue #21

--- ArticleHelper.php
+++ ArticleHelper.php
@@ -7,31 +7,34 @@ class ArticleHelper extends OntoWiki_Component_Helper
      */
     public function init()
     {
-        // Show tab only if model is selected and editable
-        $modelIsSelected = null != OntoWiki::getInstance()->selectedModel;
-        
-        if( true == $modelIsSelected ) {
+        if ('properties' == OntoWiki::getInstance()->lastRoute)
+        {
+            // Show tab only if model is selected and editable
+            $modelIsSelected = null != OntoWiki::getInstance()->selectedModel;
             
-            $modelIsEditable = OntoWiki::getInstance()->selectedModel->isEditable();
-            
-            if ( true == $modelIsEditable  ) {
-                //Ontowiki Navigation
-                $navigation = OntoWiki::getInstance()->getNavigation();
-
-                // get current request info
-                $request  = Zend_Controller_Front::getInstance()->getRequest();
-                $controller = $request->getControllerName();
-                $action = $request->getActionName();
+            if( true == $modelIsSelected ) {
                 
-                // set standard priority
-                $standardPriority = $this->_privateConfig->get('standardPriority');
+                $modelIsEditable = OntoWiki::getInstance()->selectedModel->isEditable();
                 
-                $navigation->register('article', array(
-                    'controller' => 'article',     
-                    'action'     => 'edit',        
-                    'name'       => 'Article',
-                    'priority'   => $standardPriority
-                ));
+                if ( true == $modelIsEditable ) {
+                    //Ontowiki Navigation
+                    $navigation = OntoWiki::getInstance()->getNavigation();
+    
+                    // get current request info
+                    $request  = Zend_Controller_Front::getInstance()->getRequest();
+                    $controller = $request->getControllerName();
+                    $action = $request->getActionName();
+                    
+                    // set standard priority
+                    $standardPriority = $this->_privateConfig->get('standardPriority');
+                    
+                    $navigation->register('article', array(
+                        'controller' => 'article',     
+                        'action'     => 'edit',        
+                        'name'       => 'Article',
+                        'priority'   => $standardPriority
+                    ));
+                }
             }
         }
     }
