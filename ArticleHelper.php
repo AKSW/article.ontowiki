d345c6e0acfd03b1c5061d591f6f5ccb877fd50d
Lars Eidam <larseidam@googlemail.com>
2013-01-15 16:39:28 +0100

    fix little bug if no model is selected
    - if no model was selected the articleHelper cause an error

--- ArticleHelper.php
+++ ArticleHelper.php
@@ -58,37 +58,39 @@ class ArticleHelper extends OntoWiki_Component_Helper
 
         if ("" != $request->getParam('r')) {
             $currentResource = $request->getParam('r');
-            $currentResourceClasses = OntoWiki::getInstance()->selectedModel->sparqlQuery(
-                'SELECT ?uri
-                WHERE {
-                    <' . $currentResource . '>
-                        <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?uri.
-                };'
-            );
-
-            foreach ($currentResourceClasses as $currentResourceClass) {
-                if ($currentResourceClass['uri'] == $viewOnClass) {
-                    $foundViewOnClass = true;
-                    break;
-                }
-            }
-
-            if (true == $foundViewOnClass) {
-                // get current route info
-                $front  = Zend_Controller_Front::getInstance();
-                $router = $front->getRouter();
-
-                // we must set a new route so that the navigation class knows,
-                $route = new Zend_Controller_Router_Route(
-                    'resource/properties',         // hijack 'resource/properties' shortcut
-                    array(
-                        'controller' => 'article', // map to 'semanticsitemap' controller and
-                        'action'     => 'edit'     // 'sitemap' action
-                    )
+            if ("" != $currentResource && isset(OntoWiki::getInstance()->selectedModel)) {
+                $currentResourceClasses = OntoWiki::getInstance()->selectedModel->sparqlQuery(
+                    'SELECT ?uri
+                    WHERE {
+                        <' . $currentResource . '>
+                            <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?uri.
+                    };'
                 );
-                $route->setMatchedPath('resource/properties');
-                // add the new route
-                $router->addRoute('article', $route);
+
+                foreach ($currentResourceClasses as $currentResourceClass) {
+                    if ($currentResourceClass['uri'] == $viewOnClass) {
+                        $foundViewOnClass = true;
+                        break;
+                    }
+                }
+
+                if (true == $foundViewOnClass) {
+                    // get current route info
+                    $front  = Zend_Controller_Front::getInstance();
+                    $router = $front->getRouter();
+
+                    // we must set a new route so that the navigation class knows,
+                    $route = new Zend_Controller_Router_Route(
+                        'resource/properties',         // hijack 'resource/properties' shortcut
+                        array(
+                            'controller' => 'article', // map to 'semanticsitemap' controller and
+                            'action'     => 'edit'     // 'sitemap' action
+                        )
+                    );
+                    $route->setMatchedPath('resource/properties');
+                    // add the new route
+                    $router->addRoute('article', $route);
+                }
             }
         }
     }
